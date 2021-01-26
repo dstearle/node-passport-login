5a4f4e6f4712c38845222d236479e46aa3e6e3be
dstearle <dallas.st.earle@gmail.com>
2021-01-26 09:58:24 -0800

    Setup express-ejs-layouts in main app

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // Imports
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 // Initialize Express
 const app = express();
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', require('./routes/index'));
