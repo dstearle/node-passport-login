a54fa1ee7110086bb45bd0eb598bf2ddbc2d0bcf
dstearle <dallas.st.earle@gmail.com>
2021-01-26 13:37:56 -0800

    Setup basic form validation for the registration form

--- routes/users.js
+++ routes/users.js
@@ -11,8 +11,54 @@ router.get('/register', (req, res) => res.render('register'));
 // Register Handler
 router.post('/register', (req, res) => {
 
-    console.log(req.body);
-    res.send('hello');
+    // The values to be pulled from the fields
+    const { name, email, password, password2 } = req.body;
+
+    // An array to hold error messages
+    let errors = [];
+
+    // Check required fields
+    if(!name || !email || !password || !password2) {
+
+        errors.push({ msg: 'Please fill in all fields!' });
+
+    }
+
+    // Check if passwords match
+    if(password !== password2) {
+
+        errors.push({ msg: 'Passwords do not match!'});
+
+    }
+
+    // Check password length
+    if(password.length < 6) {
+
+        errors.push({ msg: 'Password needs to be longer than 6 characters!'});
+
+    }
+
+    // If there are any errors...
+    if(errors.length > 0) {
+        
+        res.render('register', {
+
+            errors,
+            name,
+            email,
+            password,
+            password2
+            
+        });
+
+    }
+
+    // Else submit the field
+    else {
+
+        res.send('pass');
+
+    }
 
 });
 
