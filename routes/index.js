a237b4aaca970505e02ee9f9cde96450a05f1dee
dstearle <dallas.st.earle@gmail.com>
2021-01-27 13:59:25 -0800

    Implemented ensureAuthenticated into index for routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 // Imports
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 // Home Page
 router.get('/', (req, res) => res.render('welcome'));
 // Dashboard Page
-router.get('/dashboard', (req, res) => res.render('dashboard'));
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard'));
 
 module.exports = router;
