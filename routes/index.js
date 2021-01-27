ea0c5de903dc36c76c88d870f04f8d4a4cea34f9
dstearle <dallas.st.earle@gmail.com>
2021-01-27 14:03:35 -0800

    Added user name to show for the dashboard after authentication

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const { ensureAuthenticated } = require('../config/auth');
 // Home Page
 router.get('/', (req, res) => res.render('welcome'));
 // Dashboard Page
-router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard'));
+router.get('/dashboard', ensureAuthenticated, (req, res) => 
+    res.render('dashboard', {
+        name: req.user.name
+    }));
 
 module.exports = router;
