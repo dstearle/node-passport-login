99c8804b9961fa490dbb1dfe7fd484a728fbd7a4
dstearle <dallas.st.earle@gmail.com>
2021-01-27 13:37:57 -0800

    Setup route for dashboard

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 // Home Page
 router.get('/', (req, res) => res.render('welcome'));
+// Dashboard Page
+router.get('/dashboard', (req, res) => res.render('dashboard'));
 
 module.exports = router;
